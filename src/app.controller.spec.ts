2f3f7920b197bb4bcf646141325b3902dcd75276
Manjeetmathur <manjeetkumar62054@gmail.com>
2026-07-19 16:25:12 +0530

    manjeet Kumar contact

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World! why why why why"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+      expect(appController.getHello()).toBe('Hello World! thk');
     });
   });
 });
